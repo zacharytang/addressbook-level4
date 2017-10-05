bffcc6c510d0526c1faa10d2480ece440036beeb
Zachary Tang <zacharytang.tc@gmail.com>
2017-10-05 19:36:39 +0800

    Commands: Added secondary command keywords

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -33,6 +33,7 @@ public class EditCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "edit";
     public static final String COMMAND_ALIAS = "e";
+    public static final String COMMAND_SECONDARY = "modify";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
             + "by the index number used in the last person listing. "
--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -9,6 +9,7 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
     public static final String COMMAND_ALIAS = "l";
+    public static final String COMMAND_SECONDARY = "showall";
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
