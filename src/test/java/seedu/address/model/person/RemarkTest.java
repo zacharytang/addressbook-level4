33d1ae312eb6ac8ae47d73b3ee015953a0b2ee9c
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-10-09 22:33:36 +0800

    Add test for Remark, to test the Remark#equals() method.

---
+++ src/test/java/seedu/address/model/person/RemarkTest.java
@@ -0,0 +1,36 @@
+package seedu.Remark.model.person;
+
+import org.junit.Test;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Remark;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RemarkTest {
+
+    @Test
+    public void equals() {
+        Remark likeCoffeeRemark = new Remark("Likes to drink coffee.");
+        Remark capFiveRemark = new Remark("Got CAP 5.0.");
+
+        // same object -> returns true
+        assertTrue(likeCoffeeRemark.equals(likeCoffeeRemark));
+
+        // same object -> returns true
+        assertTrue(capFiveRemark.equals(capFiveRemark));
+
+        // different types -> returns false
+        assertFalse(capFiveRemark.equals(1));
+
+        // null -> returns false
+        assertFalse(capFiveRemark.equals(null));
+
+        // different objects -> returns false
+        assertFalse(likeCoffeeRemark.equals(capFiveRemark));
+    }
+}
