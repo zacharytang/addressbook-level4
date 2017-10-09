2271045fc32a3a8a5b4e05e41b5ba5117f118f6e
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-10-09 20:48:19 +0800

    Add RemarkCommandParser that knows how to parse two arguments, one index and one with prefix 'r/'.

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -21,19 +21,19 @@ public class RemarkCommandParser implements Parser<RemarkCommand>{
      */
     public RemarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
 
         Index index;
 
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            Remark remark = ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK)).get();
+
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE));
         }
-
-        RemarkCommand.RemarkPersonDescriptor remarkPersonDescriptor = new RemarkCommand.RemarkPersonDescriptor();
-        try {
+        
+         try {
             ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK)).ifPresent(remarkPersonDescriptor::setRemark);
 
         } catch (IllegalValueException ive) {
@@ -44,6 +44,6 @@ public class RemarkCommandParser implements Parser<RemarkCommand>{
             //do something
         }
 
-        return new RemarkCommand(index, remarkPersonDescriptor);
+        return new RemarkCommand(index, remarkMsg);
     }
 }
