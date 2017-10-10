a6de7407c12dddde194ebcf71a4d6c286b3b91b9
YuHsuan <cindy1.tsai@gmail.com>
2017-10-10 23:13:19 +0800

    Changed error message colour

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -12,6 +12,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.NewResultAvailableEvent;
+import javafx.collections.ObservableList;
 
 /**
  * A ui for the status bar that is displayed at the header of the application.
@@ -36,6 +37,18 @@ public class ResultDisplay extends UiPart<Region> {
     private void handleNewResultAvailableEvent(NewResultAvailableEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
         Platform.runLater(() -> displayed.setValue(event.message));
+
+        if(event.isError) setErrorStyle();
+        else setDefaultStyle();
+    }
+
+    private void setDefaultStyle() {resultDisplay.getStyleClass().remove("error");}
+
+    private void setErrorStyle() {
+        ObservableList<String> style = resultDisplay.getStyleClass();
+        if(style.contains("error")) return;
+
+        style.add("error");
     }
 
 }
