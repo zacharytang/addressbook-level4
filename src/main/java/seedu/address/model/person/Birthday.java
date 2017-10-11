1e150dd9b40f4e16ff84fe76447a3ae12323f958
YuHsuan <cindy1.tsai@gmail.com>
2017-10-11 08:18:59 +0800

    Created birthday attribute (on-going)

---
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -0,0 +1,8 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+public class Birthday {
+}
