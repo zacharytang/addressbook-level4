4538b4f43ba9e3e0e16d2e36d5405da9dcbe031e
Zachary Tang <zacharytang.tc@gmail.com>
2017-10-15 16:07:11 +0800

    Fixed checkstyle error

--- src/test/java/seedu/address/commons/util/TimetableParserUtilTest.java
+++ src/test/java/seedu/address/commons/util/TimetableParserUtilTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.commons.util.TimetableParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.timetable.TimetableWeek;
 
