298ad8437f4283fbad145a84a19798df56604292
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-10-16 23:26:57 +0800

    Model: add Gender class

---
+++ src/main/java/seedu/address/model/person/Gender.java
@@ -0,0 +1,57 @@
+package seedu.address.model.person;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+
+/**
+ * Represents a Person's gender in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidGender(String)}
+ */
+public class Gender {
+
+    public static final String MESSAGE_GENDER_CONSTRAINTS =
+            "Person gender should be a string of either 'Male' or 'Female'";
+    public static final String GENDER_VALIDATION_WORD1 = "Male";
+    public static final String GENDER_VALIDATION_WORD2 = "Female";
+
+    public final String value;
+
+    /**
+     * Validates given gender.
+     *
+     * @throws IllegalValueException if given gender string is invalid.
+     */
+    public Gender(String gender) throws IllegalValueException {
+        requireNonNull(gender);
+        String trimmedGender = gender.trim();
+        if (!isValidGender(trimmedGender)) {
+            throw new IllegalValueException(MESSAGE_GENDER_CONSTRAINTS);
+        }
+        this.value = trimmedGender;
+    }
+
+    /**
+     * Returns if a given string is a valid person gender.
+     */
+    public static boolean isValidGender(String test) {
+        return test.equals(GENDER_VALIDATION_WORD1) || test.equals(GENDER_VALIDATION_WORD2);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Gender // instanceof handles nulls
+                && this.value.equals(((Gender) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
