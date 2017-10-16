9048ff092be41da0e9878bf5dd2a5f69edfb73f8
nbriannl <neilbrian.nl@gmail.com>
2017-10-16 15:13:13 +0800

    "CliSyntax: added old and new prefixes"

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,5 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_OLD_TAG = new Prefix("old/");
+    public static final Prefix PREFIX_NEW_TAG = new Prefix("new/");
 
 }
