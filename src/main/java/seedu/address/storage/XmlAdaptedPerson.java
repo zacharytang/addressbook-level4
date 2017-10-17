bb3d1c8c32dddd7a19ed0eea02e58a1fa6fbb396
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-10-17 08:46:42 +0800

    Storage: modify XmlAdaptedPerson

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -8,13 +8,7 @@ import java.util.Set;
 import javax.xml.bind.annotation.XmlElement;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.Remark;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -24,6 +18,10 @@ public class XmlAdaptedPerson {
 
     @XmlElement(required = true)
     private String name;
+    @XmlElement(required = true)
+    private String gender;
+    @XmlElement(required = true)
+    private String matricNo;
     @XmlElement(required = true)
     private String phone;
     @XmlElement(required = true)
@@ -50,6 +48,8 @@ public class XmlAdaptedPerson {
      */
     public XmlAdaptedPerson(ReadOnlyPerson source) {
         name = source.getName().fullName;
+        gender = source.getGender().value;
+        matricNo = source.getMatricNo().value;
         phone = source.getPhone().value;
         email = source.getEmail().value;
         address = source.getAddress().value;
@@ -71,11 +71,13 @@ public class XmlAdaptedPerson {
             personTags.add(tag.toModelType());
         }
         final Name name = new Name(this.name);
+        final Gender gender = new Gender(this.gender);
+        final MatricNo matricNo = new MatricNo(this.matricNo);
         final Phone phone = new Phone(this.phone);
         final Email email = new Email(this.email);
         final Address address = new Address(this.address);
         final Remark remark = new Remark(this.remark);
         final Set<Tag> tags = new HashSet<>(personTags);
-        return new Person(name, phone, email, address, remark, tags);
+        return new Person(name, gender, matricNo, phone, email, address, remark, tags);
     }
 }
