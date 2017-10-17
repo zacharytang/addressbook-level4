a5397d5ae29fd3ddbfd85ba58686feb1337cb93e
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-10-17 08:39:20 +0800

    Model: update SampleDataUtil

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -6,8 +6,11 @@ import java.util.Set;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
+
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Gender;
+import seedu.address.model.person.MatricNo;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -22,22 +25,28 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         try {
             return new Person[]{
-                new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                new Person(new Name("Alex Yeoh"), new Gender("Male"), new MatricNo("A0162531N"),
+                        new Phone("87438807"), new Email("alexyeoh@example.com"),
                         new Address("Blk 30 Geylang Street 29, #06-40"), new Remark(""),
                         getTagSet("friends")),
-                new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                new Person(new Name("Bernice Yu"), new Gender("Male"), new MatricNo("A0162541N"),
+                        new Phone("99272758"), new Email("berniceyu@example.com"),
                         new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Remark("Likes to drink tea"),
                         getTagSet("colleagues", "friends")),
-                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+                new Person(new Name("Charlotte Oliveiro"), new Gender("Female"), new MatricNo("A0163331N"),
+                        new Phone("93210283"), new Email("charlotte@example.com"),
                         new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Remark(""),
                         getTagSet("neighbours")),
-                new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                new Person(new Name("David Li"), new Gender("Male"), new MatricNo("A0165231N"),
+                        new Phone("91031282"), new Email("lidavid@example.com"),
                         new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Remark("Likes to drink coffee"),
                         getTagSet("family")),
-                new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+                new Person(new Name("Irfan Ibrahim"), new Gender("Male"), new MatricNo("A0175531N"),
+                        new Phone("92492021"), new Email("irfan@example.com"),
                         new Address("Blk 47 Tampines Street 20, #17-35"), new Remark(""),
                         getTagSet("classmates")),
-                new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+                new Person(new Name("Roy Balakrishnan"), new Gender("Male"), new MatricNo("A0155531K"),
+                        new Phone("92624417"), new Email("royb@example.com"),
                         new Address("Blk 45 Aljunied Street 85, #11-31"), new Remark("CAP 5.0"),
                         getTagSet("colleagues"))
             };
