b3c33cbdbab3753eeadef7de91823d0294151b17
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-10-21 15:33:34 +0800

    update deleteCommand

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -16,7 +16,7 @@ import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.exceptions.TagNotFoundException;
 
 /**
- * Deletes a person identified using it's last displayed index from the address book,
+ * Deletes persons identified using their last displayed indexes from the address book,
  * or a tag identified by the tag name
  */
 public class DeleteCommand extends UndoableCommand {
@@ -26,50 +26,55 @@ public class DeleteCommand extends UndoableCommand {
     public static final String COMMAND_SECONDARY = "remove";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the last person listing.\n"
+            + ": Deletes persons identified using their last displayed indexes used in the last person listing.\n"
             + "  OR the tag specified from all people containing the specific tag\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Parameters: INDEX (must be positive integers)\n"
             + "        OR  " + PREFIX_TAG + "TAG (case-sensitive)\n "
             + "Example: " + COMMAND_WORD + " 1\n"
+            + "         " + COMMAND_WORD + " 1, 2, 3\n"
             + "         " + COMMAND_WORD + " " + PREFIX_TAG + "friend\n"
             + "         " + COMMAND_WORD + " " + PREFIX_TAG + "friend " + PREFIX_TAG + "enemy";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    //need to check here
+    //public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Persons";
     public static final String MESSAGE_DELETE_TAG_SUCCESS = "Deleted Tags";
 
-    private final Index targetIndex;
+    private final ArrayList<Index> targetIndexes;
 
     private final Set<Tag> targetTags;
 
-    public DeleteCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    public DeleteCommand(ArrayList<Index> targetIndexes) {
+        this.targetIndexes = targetIndexes;
         this.targetTags = null;
     }
 
     public DeleteCommand(Set<Tag> targetTags) {
+        this.targetIndexes = null;
         this.targetTags = targetTags;
-        this.targetIndex = null;
     }
 
     @Override
     public CommandResult executeUndoableCommand() throws CommandException {
-        // this code block is command execution for delete [index]
-        if (targetTags == null && targetIndex != null) {
+        // this code block is command execution for delete [indexes]
+        if (targetTags == null && targetIndexes != null) {
             List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
 
-            if (targetIndex.getZeroBased() >= lastShownList.size()) {
-                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            for (Index index : targetIndexes) {
+                if (index.getZeroBased() >= lastShownList.size()) {
+                    throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+                }
+                ReadOnlyPerson personToDelete = lastShownList.get(index.getZeroBased());
+
+                try {
+                    model.deletePerson(personToDelete);
+                } catch (PersonNotFoundException pnfe) {
+                    assert false : "The target person with index " + index.getOneBased() + "cannot be missing";
+                }
             }
 
-            ReadOnlyPerson personToDelete = lastShownList.get(targetIndex.getZeroBased());
+            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS));
 
-            try {
-                model.deletePerson(personToDelete);
-            } catch (PersonNotFoundException pnfe) {
-                assert false : "The target person cannot be missing";
-            }
-
-            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, personToDelete));
         } else { // this code block is command execution for delete t/[tag...]
             ArrayList<Tag> arrayTags = new ArrayList<Tag>(targetTags);
             List<Tag> listOfExistingTags = model.getAddressBook().getTagList();
@@ -96,10 +101,10 @@ public class DeleteCommand extends UndoableCommand {
 
     @Override
     public boolean equals(Object other) {
-        if (targetTags == null) {
+        if (targetIndexes != null) {
             return other == this // short circuit if same object
                     || (other instanceof DeleteCommand // instanceof handles nulls
-                    && this.targetIndex.equals(((DeleteCommand) other).targetIndex)); // state check
+                    && this.targetIndexes.equals(((DeleteCommand) other).targetIndexes)); // state check
         } else {
             return other == this // short circuit if same object
                     || (other instanceof DeleteCommand // instanceof handles nulls
