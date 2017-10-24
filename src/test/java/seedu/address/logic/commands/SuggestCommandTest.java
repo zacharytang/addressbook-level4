ec352c760b9c1c58c647dd3615509987f918e297
YuHsuan <cindy1.tsai@gmail.com>
2017-10-24 21:14:05 +0800

    Create tests for SuggestCommand and parser

---
+++ src/test/java/seedu/address/logic/commands/SuggestCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static seedu.address.logic.commands.SuggestCommand.MESSAGE_SUCCESS;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code SuggestCommand}.
+ */
+public class SuggestCommandTest {
+    @Test
+    public void execute_suggest_success() {
+        String command = "edit";
+        String expectedMessage = String.format(MESSAGE_SUCCESS, command);
+
+        // assert that suggest command is executed
+        try {
+            new SuggestCommand(command).execute();
+            fail("The expected CommandException was not thrown.");
+        } catch (CommandException e) {
+            assertEquals(expectedMessage, e.getMessage());
+        }
+    }
+}
+
