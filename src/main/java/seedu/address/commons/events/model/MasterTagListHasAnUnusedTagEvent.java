91ed1bf7e3ac581bcacdd4cd45f73f066d40e285
nbriannl <neilbrian.nl@gmail.com>
2017-10-29 15:51:39 +0800

    "New event to say that the master tag list has unused tags"

---
+++ src/main/java/seedu/address/commons/events/model/MasterTagListHasAnUnusedTagEvent.java
@@ -0,0 +1,21 @@
+package seedu.address.commons.events.model;
+
+import java.util.Set;
+
+import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.tag.Tag;
+
+/** Indicates that the master tag list has an unused tag not used by any person*/
+public class MasterTagListHasAnUnusedTagEvent extends BaseEvent {
+
+    public final Set<Tag> outdatedTags;
+
+    public MasterTagListHasAnUnusedTagEvent (Set<Tag> outdatedTags) {
+        this.outdatedTags = outdatedTags;
+    }
+
+    @Override
+    public String toString() {
+        return "The tag list is outdated. With outdated tags: " + outdatedTags.toString();
+    }
+}
