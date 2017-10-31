502d5209fb64385e737f9c6e3bcb3977a280b1ae
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-10-31 21:45:19 +0800

    display contactPhotos

--- src/main/java/seedu/address/ui/PersonInfoOverview.java
+++ src/main/java/seedu/address/ui/PersonInfoOverview.java
@@ -1,5 +1,8 @@
 package seedu.address.ui;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
@@ -7,6 +10,8 @@ import com.google.common.eventbus.Subscribe;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonSelectedEvent;
@@ -38,6 +43,8 @@ public class PersonInfoOverview extends UiPart<Region> {
     private Label birthday;
     @FXML
     private Label remark;
+    @FXML
+    private ImageView contactPhoto;
 
     public PersonInfoOverview() {
         super(FXML);
@@ -60,6 +67,7 @@ public class PersonInfoOverview extends UiPart<Region> {
         email.setText("");
         birthday.setText("");
         remark.setText("");
+        setDefaultContactPhoto();
     }
 
     /**
@@ -76,6 +84,31 @@ public class PersonInfoOverview extends UiPart<Region> {
         email.textProperty().bind(Bindings.convert(person.emailProperty()));
         birthday.textProperty().bind(Bindings.convert(person.birthdayProperty()));
         remark.textProperty().bind(Bindings.convert(person.remarkProperty()));
+        loadPhoto(person);
+    }
+
+    /**
+     * Set the default contact photo to the default person.
+     */
+    public void setDefaultContactPhoto() {
+        String defaultPhotoPath = "src/main/resources/images/help_icon.png";
+        File defaultPhoto = new File(defaultPhotoPath);
+        URI defaultPhotoUri = defaultPhoto.toURI();
+        Image defaultImage = new Image(defaultPhotoUri.toString());
+        contactPhoto.setImage(defaultImage);
+    }
+
+    /**
+     * Load the photo of the specified person.
+     * @param person
+     */
+    public void loadPhoto(ReadOnlyPerson person) {
+        String photoPath = person.getPhotoPath().value;
+        File photo = new File(photoPath);
+        URI photoUri = photo.toURI();
+        Image image = new Image(photoUri.toString());
+
+        contactPhoto.setImage(image);
     }
 
     @Subscribe
