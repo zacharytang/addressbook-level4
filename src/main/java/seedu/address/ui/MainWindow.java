fb40b6a810499d01b6ae219562bd5cfa27c0c13e
nbriannl <neilbrian.nl@gmail.com>
2017-11-11 18:48:48 +0800

    Dividers are now subtle as a shadow, deleted borders

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -164,17 +164,6 @@ public class MainWindow extends UiPart<Region> {
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
     }
 
-    /**
-     *
-     */
-    public void loadBorders() {
-        personListPanelPlaceholder.setStyle("-fx-border-color:red");
-        tagListPanelPlaceholder.setStyle("-fx-border-color:red");
-        personInfoPlaceholder.setStyle("-fx-border-color:blue");
-        infoPlaceholder.setStyle("-fx-border-color:yellow");
-        commandBoxPlaceholder.setStyle("-fx-border-color:aqua");
-    }
-
     void hide() {
         primaryStage.hide();
     }
--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -57,7 +57,6 @@ public class UiManager extends ComponentManager implements Ui {
             mainWindow = new MainWindow(primaryStage, config, prefs, logic);
             mainWindow.show(); //This should be called before creating other UI parts
             mainWindow.fillInnerParts();
-            mainWindow.loadBorders();
 
         } catch (Throwable e) {
             logger.severe(StringUtil.getDetails(e));
