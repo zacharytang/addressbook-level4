a1281dd11f3c729332416d23fa0ee10d24e6b422
nbriannl <neilbrian.nl@gmail.com>
2017-11-12 13:02:44 +0800

    Throws a message if tag exceeds 25 characters

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -3,7 +3,9 @@ package seedu.address.model.tag;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemarkCommand;
 
+//@@author nbriannl
 /**
  * Represents a Tag in the address book.
  * Guarantees: immutable; name is valid as declared in {@link #isValidTagName(String)}
@@ -12,6 +14,7 @@ public class Tag {
 
     public static final String MESSAGE_TAG_CONSTRAINTS = "Tags names should be alphanumeric, and should not be blank";
     public static final String TAG_VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final int TAG_ACCEPTABLE_LENGTH = 25;
 
     public final String tagName;
 
@@ -26,6 +29,9 @@ public class Tag {
         if (!isValidTagName(trimmedName)) {
             throw new IllegalValueException(MESSAGE_TAG_CONSTRAINTS);
         }
+        if (!isAcceptableTagLength(trimmedName)) {
+            throw new IllegalValueException(generateExceptionMessageForLongTag(trimmedName));
+        }
         this.tagName = trimmedName;
     }
 
@@ -34,6 +40,24 @@ public class Tag {
      */
     public static boolean isValidTagName(String test) {
         return test.matches(TAG_VALIDATION_REGEX);
+    }
+
+    /**
+     * Returns true if a valid tag name is too long.
+     */
+    public static boolean isAcceptableTagLength(String test) {
+        return test.length() < TAG_ACCEPTABLE_LENGTH;
+    }
+
+    /**
+     * Generates the exception message when trying to create a tag which is too long
+     */
+    public static String generateExceptionMessageForLongTag(String longTag) {
+        return "The tag: " + longTag + " is too long!\n"
+                + "Consider adding a remark instead?\n"
+                + "Example: remark INDEX r/" + longTag + "\n"
+                + "\n"
+                + RemarkCommand.MESSAGE_USAGE;
     }
 
     @Override
